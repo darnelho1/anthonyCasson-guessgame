784713196caa20c397c4e49223129c83357566c0
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2015-11-02 23:50:15 -0800

    Add question four, adjust total script to account for six questions, link html to appropriate js file

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 
 /********** GUESSING GAME SCRIPTS by ANTHONY S. CASSON **********/
 
-/*****
-Script for the FIRST question
-*****/
+
+//Script for the FIRST question
+
 var userResponse1 = prompt('Do I drive a sweet 1986 Volvo?');
 var responseScore1; //global var modified inside 'if'
 
@@ -17,9 +17,9 @@ if ((userResponse1 === 'yes')||(userResponse1 === 'Yes')||(userResponse1 === 'YE
 
 
 
-/*****
-Script for the SECOND question
-*****/
+
+// Script for the SECOND question
+
 var userResponse2 = prompt('Did I grow up in Texas?');
 var responseScore2; //global var modified inside 'if'
 
@@ -33,9 +33,9 @@ if ((userResponse2 === 'yes')||(userResponse2 === 'Yes')||(userResponse2 === 'YE
 
 
 
-/*****
-Script for the THIRD question
-*****/
+
+// Script for the THIRD question
+
 var userResponse3 = prompt('Is my favorite color green?');
 var responseScore3; //global var modified inside 'if'
 
@@ -47,14 +47,29 @@ if ((userResponse3 === 'no')||(userResponse3 === 'No')||(userResponse3 === 'NO')
   responseScore3 = 0;
 };
 
-/*****
-Script for the TOTAL answers correct
-*****/
-var totalCorrect = responseScore1 + responseScore2 + responseScore3;
 
-if (totalCorrect >= 2) { //majority reaction
-  console.log("You got " + totalCorrect + " out of 3 answers correct! Nice work!");
+// Script for the FOURTH question
+
+var userResponse4 = prompt('Did I go to college?');
+var responseScore4; //global var modified inside 'if'
+
+if ((userResponse4 === 'yes')||(userResponse4 === 'Yes')||(userResponse4 === 'YES')||(userResponse4 === 'Y')||(userResponse4 === 'y')) {
+  console.log('Correct! I graduated from Oregon State University.');
+  responseScore4 = 1;
+}else {
+  console.log('Incorrect. I graduated from Oregon State University');
+  responseScore4 = 0;
+};
+
+
+
+// Script for the TOTAL answers correct
+
+var totalCorrect = responseScore1 + responseScore2 + responseScore3 + responseScore4;
+
+if (totalCorrect >= 4) { //majority reaction
+  console.log("You got " + totalCorrect + " out of 6 answers correct! Nice work!");
 } else { //minority reaction
-  console.log("You only got " + totalCorrect + " out of 3 answers correct! Better luck next time!");
+  console.log("You only got " + totalCorrect + " out of 6 answers correct! Better luck next time!");
 };
 
